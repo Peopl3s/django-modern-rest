9f46bc2831f48f6646757ee010d36644cc2a0dee
sobolevn <mail@sobolevn.me>
2026-04-05 22:10:40 +0300

    Run schemathesis without our response validation (#825)
    * Run schemathesis without our response validation
    
    * Run schemathesis without our response validation
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- dmr/validation/response.py
+++ dmr/validation/response.py
@@ -12,7 +12,6 @@ from dmr.exceptions import (
     ValidationError,
 )
 from dmr.files import FileBody
-from dmr.headers import build_headers
 from dmr.internal.negotiation import (
     media_by_precedence,
     response_validation_negotiator,
@@ -56,7 +55,7 @@ class ResponseValidator:  # noqa: WPS214
         response: _ResponseT,
     ) -> _ResponseT:
         """Validate response based on provided schema."""
-        if not self.metadata.validate_responses:
+        if not self._should_validate_responses():
             return response
         schema = self._get_response_schema(response.status_code)
         renderer = request_renderer(
@@ -104,14 +103,11 @@ class ResponseValidator:  # noqa: WPS214
         all_response_data = ValidatedModification(
             raw_data=structured,
             status_code=self.metadata.modification.status_code,
-            headers=build_headers(
-                self.metadata.modification,
-                renderer,
-            ),
+            headers=self.metadata.modification.build_headers(renderer),
             cookies=self.metadata.modification.actionable_cookies(),
             renderer=renderer,
         )
-        if not self.metadata.validate_responses:
+        if not self._should_validate_responses():
             return all_response_data
         schema = self._get_response_schema(all_response_data.status_code)
         self._validate_body(
@@ -120,6 +116,9 @@ class ResponseValidator:  # noqa: WPS214
             content_type=renderer.content_type,
         )
         return all_response_data
+
+    def _should_validate_responses(self) -> bool:
+        return self.metadata.validate_responses is True
 
     def _get_response_schema(
         self,
