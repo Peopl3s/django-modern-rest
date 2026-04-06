11e2c5f29c170022333bb7b708d0216cae81608a
sobolevn <mail@sobolevn.me>
2026-04-06 20:53:57 +0300

    Decrease the number of jobs in PR CI (#840)

--- .github/workflows/build-pr-matrix.sh
+++ .github/workflows/build-pr-matrix.sh
@@ -2,10 +2,9 @@
 
 set -euo pipefail
 
-export CIBW_ARCHS_LINUX='x86_64'
-
 {
-  CIBW_BUILD="cp311-*" cibuildwheel --print-build-identifiers --platform linux \
+  CIBW_ARCHS_LINUX='x86_64' CIBW_SKIP='*-musllinux_*' CIBW_BUILD="cp311-*" \
+    cibuildwheel --print-build-identifiers --platform linux \
   | pyp 'json.dumps({"only": x, "os": "ubuntu-latest"})' \
   && CIBW_BUILD="cp312-*" cibuildwheel --print-build-identifiers --platform windows \
   | pyp 'json.dumps({"only": x, "os": "windows-latest"})' \
